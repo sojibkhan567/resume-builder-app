3f8bb673e8b4e670f7d2194a96895f393caa7dd2
Sajeeb Khan <sbhossain567@gmail.com>
2026-01-02 23:38:30 +0600

    feat-02: add dashboard page

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  const user = {name: "Sajeeb Khan"}
+
+  return (
+    <div className='shadow bg-white'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
+            <Link to="/">
+                <img className='h-11 w-auto' src="/logo.svg" alt="logo" />
+            </Link>
+            <div className='flex items-center gap-4 text-sm'>
+                <p className='max-sm:hidden'>Hi, {user?.name}</p>
+                <button className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout</button>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import {dummyResumeData} from "../assets/assets";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
 
-export default Dashboard
+  const [allResumes, setAllResumes] = useState([]);
+
+  // fetch all resume data
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData)
+  }
+
+  // call fetch api method
+  useEffect(() => {
+    loadAllResumes();
+  },[]);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <p className="text-2xl font-medium mb-6 bg-linear-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
+          Welcome, Jhon doe
+        </p>
+        {/* create resume btn */}
+        <div className="flex gap-4 ">
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-indigo-300 to-indigo-500  text-white rounded-full"/>
+            <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Create Resume
+            </p>
+          </button>
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-purple-300 to-purple-500  text-white rounded-full"/>
+            <p className="text-sm group-hover:text-purple-600 transition-all duration-300">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+        <hr className="my-6 border-slate-300"/>
+        {/* resume list */}
+        <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length];
+            return(
+              <button key={index} className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300" style={{ background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: baseColor + '40' }}>
+
+              </button>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
