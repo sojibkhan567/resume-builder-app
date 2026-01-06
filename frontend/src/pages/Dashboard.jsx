458d9dd4ce921fb33ba0187f7802e7851b167f68
Sajeeb Khan <sbhossain567@gmail.com>
2026-01-06 10:48:06 +0600

    f-04: enhance PersonalInfoForm with additional fields and improve layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -135,9 +135,8 @@ const Dashboard = () => {
               <input
                 placeholder="Enter resume title"
                 className="w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600"
-                required=""
+                required
                 type="text"
-                value=""
               />
               <button className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors">
                 Create Resume
--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { dummyResumeData } from "../assets/assets";
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkles,
+  User,
+} from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
 
+  // initialize resume data
   const [resumeData, setResumeData] = useState({
     _id: " ",
     title: "",
@@ -29,7 +41,7 @@ const ResumeBuilder = () => {
     }
   };
 
-  // manage section navigation states
+  /*===== Manage section navigation states =======*/
   const [activeSectionIndex, setActiveSectionIndex] = useState(0);
   const [removeBackground, setremoveBackground] = useState(false);
   // sections information list array
@@ -41,7 +53,7 @@ const ResumeBuilder = () => {
     { id: "projects", name: "Projects", icon: FolderIcon },
     { id: "skills", name: "Skills", icon: Sparkles },
   ];
-  const activeSection = sections[activeSectionIndex]
+  const activeSection = sections[activeSectionIndex];
 
   useEffect(() => {
     loadExistingResume();
@@ -50,7 +62,10 @@ const ResumeBuilder = () => {
   return (
     <>
       <div className="max-w-7xl mx-auto px-4 py-6">
-        <Link to={'/app'} className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
           <ArrowLeftIcon className="size-4" /> Back to Dashboard
         </Link>
       </div>
@@ -60,21 +75,60 @@ const ResumeBuilder = () => {
           <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
             <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
               {/* Progress bar using active section index */}
-              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200"/>
-              <hr className="absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-2000" style={{ width: `${activeSectionIndex * 100 / (sections.length - 1)}%` }}/>
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-2000"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (sections.length - 1)
+                  }%`,
+                }}
+              />
               {/* section navigation */}
               <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
                 <div></div>
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
-                    <button onClick={() => setActiveSectionIndex((preIndex)=> Math.max(preIndex - 1, 0))} className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all" disabled={activeSectionIndex === 0}>
+                    <button
+                      onClick={() =>
+                        setActiveSectionIndex((preIndex) =>
+                          Math.max(preIndex - 1, 0)
+                        )
+                      }
+                      className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all"
+                      disabled={activeSectionIndex === 0}
+                    >
                       <ChevronLeft className="size-4" /> Previous
                     </button>
                   )}
-                  <button onClick={() => setActiveSectionIndex((preIndex)=> Math.min(preIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
-                     Next <ChevronRight className="size-4" /> 
-                    </button>
+                  <button
+                    onClick={() =>
+                      setActiveSectionIndex((preIndex) =>
+                        Math.min(preIndex + 1, sections.length - 1)
+                      )
+                    }
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${
+                      activeSectionIndex === sections.length - 1 && "opacity-50"
+                    }`}
+                    disabled={activeSectionIndex === sections.length - 1}
+                  >
+                    Next <ChevronRight className="size-4" />
+                  </button>
                 </div>
+              </div>
+              {/* form content */}
+              <div className="space-y-6">
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        personal_info: data,
+                      }))
+                    }
+                  />
+                )}
               </div>
             </div>
           </div>
