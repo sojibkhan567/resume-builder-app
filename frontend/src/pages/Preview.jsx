196e6b5620b6cca51b05860f7dc6fcb7b802ccbf
Sajeeb Khan <sbhossain567@gmail.com>
2026-01-10 01:28:41 +0600

    feat: enhance Preview component with loading state and error handling

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets';
 import ResumePreview from '../components/ResumePreview';
+import { ArrowLeftIcon, Loader } from 'lucide-react';
 
 const Preview = () => {
   const {resumeId} = useParams();
 
+  const [isLoading, setLoading]= useState(true)
   const [resumeData, setResumeData] = useState(null);
 
   const loadResume = async () => {
     setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null));
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -23,7 +26,16 @@ const Preview = () => {
       </div>
     </div>
   ) : (
-    <div>h</div>
+    <div>
+      {isLoading ? <Loader /> : (
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found.</p>
+          <a href="" className='mt-6 bg-green-500 hover:bg-green-600 text-white flex items-center rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 transition-colors'>
+            <ArrowLeftIcon className='mr-2 size-4'/> Go to Home Page
+          </a>
+        </div>
+      )}
+    </div>
   )
 }
 
